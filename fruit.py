79c15ab2e8906aab36c5fbdda7ff64baf3249f73
kosuke <sheep.a08@gmail.com>
2019-09-19 16:17:12 +0900

    adress_dic の出力

---
+++ fruit.py
@@ -0,0 +1,11 @@
+def main():
+    fruit_dec = {
+        'リンゴ': 'apple',
+        'ブドウ': 'glape',
+        'みかん': 'orange',
+    }
+    print(fruit_dec['リンゴ'])
+
+
+if __name__ == '__main__':
+    main()
